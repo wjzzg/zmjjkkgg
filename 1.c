f8bcca488e0a321880fa315169496ce149ab789d
wjzzg <2790618422@qq.com>
2026-04-14 18:18:34 +0800

    上传1.c文件

---
+++ 1.c
@@ -0,0 +1,16 @@
+/*===============================================
+*   文件名称：1.c
+*   创 建 者：     
+*   创建日期：2026年04月14日
+*   描    述：
+================================================*/
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+  int i;
+  int sum;
+  char a[10] = "hello";
+  printf("大小为:%d\n", strlen(a),sizeof(a));
+  return 0;
+}
